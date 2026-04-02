adc2956c5eff6d40ce91d9a372c2e0ad06da8d1e
Angel Gomez <g.angelpat7567@gmail.com>
2026-04-02 16:49:06 -0700

    Updated Transcription

--- Transcription.py
+++ Transcription.py
@@ -10,6 +10,30 @@ DNA_BASES = {'A' : 'T', 'T' : 'A', 'G' : 'C', 'C' : 'G'}
 
 MRNA_BASES = {'A' : 'U', 'G' : 'C', 'T' : 'A', 'C' : 'G'}
 
+# DNA Polymerase Reads Template Strand (3' to 5') left to right
+
+# Then, creates mRNA strand from (5' to 3')
+
+class DNA:
+
+    def __init__(self, template_strand = "", non_template_strand = ""):
+        self._template_strand = template_strand # Runs Parallel 3' to 5'
+        self._non_template_strand = non_template_strand # Runs Anti-Parallel 5' to 3'
+        self._genome_sequence = []
+
+    def getTemplateStrand(self):
+        print("DNA (3'- 5') Template Strand: {self._template_strand}")
+
+    def getNonTemplateStrand(self):
+        print("DNA (5'- 3') Non-Template Strand: {self._non_template_strand}") 
+
+
+non_template_strand = ""
+
+template_strand = "CAGTACGCTCACGCTCACGCTCAC"
+
+
+
 
 def main():
     print("Hello, World")
